543da5c7eaa613c0a02bef1926022a052fb92e8a
PzIVAnkoSP <1834819483@qq.com>
2018-10-22 16:32:05 +0800

    commit 2

--- src/com/test/h/HelloWorld.java
+++ src/com/test/h/HelloWorld.java
@@ -2,6 +2,7 @@ package com.test.h;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld!");
+		System.out.println();
 	}
 }
